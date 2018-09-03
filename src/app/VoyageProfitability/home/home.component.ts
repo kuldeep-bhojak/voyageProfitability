81d231effa52609d87d1678b2f960fe0ec50ea4c
Kuldeep bhojak <kuldeep.bhojak@gmail.com>
2018-09-03 08:30:46 +0530

    Plan voyage div added

--- src/app/VoyageProfitability/home/home.component.ts
+++ src/app/VoyageProfitability/home/home.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { trigger, transition, animate, style } from '@angular/animations';
 
 @Component({
     selector: "home",
     templateUrl: "home.component.html",
-    styleUrls: ["home.component.css"]    
+    styleUrls: ["home.component.css"],
+    animations: [
+        trigger('slideInOut', [
+          transition(':enter', [
+            style({transform: 'translateX(0%)'}),
+            animate('200ms ease-in', style({transform: 'translateX(0%)'}))
+          ]),
+          transition(':leave', [
+            animate('200ms ease-in', style({transform: 'translateX(0%)'}))
+          ])
+        ])
+      ] 
 })
 
 export class HomeComponent{
-    
+    visible:false;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import { HttpModule  } from '@angular/http';
-
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
@@ -17,7 +16,8 @@ import { HomeComponent } from './VoyageProfitability/home/home.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
